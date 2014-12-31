25c00723714fe701cf0aebe438e0f86efcb4a9b3
Kagu-chan <kai.boese@web.de>
2015-01-01 00:08:06 +0100

    Modified loader-files for new directory pattern and multilib

--- deps/script/bin/loader.lua
+++ deps/script/bin/loader.lua
@@ -1,45 +1,49 @@
-print("LuaJit " .. _VERSION)
+print("LuaJit Version" .. _VERSION)
 
 -- Read command line arguments
-if not arg[1] or not arg[2] or not arg[3] then
-	error("Wrong usage! Usage is \n\tluajit loader.lua autoload_dir working_dir filename_base", 1)
+if not arg[1] or not arg[2] or not arg[3] or not arg[4] or not arg[5] then
+	error("Wrong usage! Usage is \n\tluajit[32|64] loader.lua autoload_dir user_autoload_dir_a user_autoload_dir_b working_dir filename_base", 1)
 end
 
-command_line_args = {}
+-- Define global list of loaded files
+_G.autoload_files = {}
+local fi = 1
 
-command_line_args.autoload = 	arg[1]
-command_line_args.working_dir = arg[2]
-command_line_args.filebase = 	arg[3]
+-- Define read_dir and load_dir
+local read_dir = require("fileutils_thin")
+local function load_dir(files)
+	local ffiles = read_dir(files)
+	for i=1, ffiles.n do
+		local file = ffiles[i]
+		if file.type == "file" and file.name:find("%.lua$") then
+			local lFile = files .. "/" .. file.name
+			autoload_files[fi] = lFile
+			dofile(lFile)
+			fi = fi + 1
+		end
+	end
+end
 
--- Define autoload directory
-autoload_path = "autoload/lua"
+_G.command_line_args = {}
 
--- Define read_dir
-read_dir = require("fileutils_thin")
+command_line_args.autoload = 			arg[1]
+command_line_args.user_autoload_dir_a = arg[2]
+command_line_args.user_autoload_dir_b = arg[3]
+command_line_args.working_dir = 		arg[4]
+command_line_args.filebase = 			arg[5]
 
 -- Autoload the autoload directory content (*.lua)
-local autoload_files = read_dir(autoload_path)
-for i=1, autoload_files.n do
-	local file = autoload_files[i]
-	if file.type == "file" and file.name:find("%.lua$") then
-		dofile(autoload_path .. "/" .. file.name)
-	end
-end
+load_dir("autoload/" .. command_line_args.autoload)
 
--- Autoload the users autoload directory (*.lua)
-local user_files = read_dir(command_line_args.autoload)
-for i=1, user_files.n do
-	local file = user_files[i]
-	if file.type == "file" and file.name:find("%.lua$") then
-		dofile(command_line_args.autoload .. "/" .. file.name)
-	end
-end
+-- Autoload the users autoload directory and library-bounded autoload directory (*.lua)
+load_dir(command_line_args.user_autoload_dir_a)
+load_dir(command_line_args.user_autoload_dir_b)
 
 -- filebase
 local f_base = command_line_args.working_dir .. "/" .. command_line_args.filebase
 
 -- Load users file
-local userfile = dofile(f_base .. ".lua")
+dofile(f_base .. ".lua")
 
 -- Initialize Environment
 _init(f_base .. ".ass", f_base .. "_out.ass")
