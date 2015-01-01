925d256c02a19be184945ed0abbeb596d9ae2578
Kagu-chan <kai.boese@web.de>
2015-01-01 23:57:18 +0100

    Added some testcode - currently luajit => ruby doesnt work.

--- deps/script/bin/interpreter.lua
+++ deps/script/bin/interpreter.lua
@@ -4,14 +4,19 @@ local ffi = require("ffi")
 -- Load Ruby library & define function headers
 local ruby = ffi.load("msvcrt-ruby210")
 ffi.cdef([[
+void ruby_init(void);
 int ruby_setup(void);
 void* ruby_options(int, char**);
 int ruby_run_node(void*);
 ]])
 
--- Test data
+-- Argument data
 local args = arg
 
+print(ruby.ruby_init)
+ruby.ruby_init()
+
+--[[
 -- Initialize Ruby
 if ruby.ruby_setup() == 0 then
 	-- Run Ruby with options
@@ -28,3 +33,4 @@ if ruby.ruby_setup() == 0 then
 else
 	print("Couldn't setup Ruby!")
 end
+]]
