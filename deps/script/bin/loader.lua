c63e158ab50404cad8d722df0a2fe39576a1f4cc
Kagu-chan <kai.boese@web.de>
2015-02-24 23:02:58 +0100

    Added the possibility to ignore autoload directories. Just leave them in programm call.

--- deps/script/bin/loader.lua
+++ deps/script/bin/loader.lua
@@ -1,8 +1,16 @@
 print("LuaJit Version" .. _VERSION)
 
 -- Read command line arguments
-if not arg[1] or not arg[2] or not arg[3] or not arg[4] or not arg[5] then
-	error("Wrong usage! Usage is \n\tluajit[32|64] loader.lua autoload_dir global_user_autoload library_user_autoload working_dir filename_base", 1)
+if (arg[1] and arg[2] and arg[3] and arg[4] and arg[5]) then
+    argument_format = 1
+elseif (arg[1] and arg[2] and arg[3] and not arg[4] and not arg[5]) then
+    argument_format = 2
+else
+    argument_format = 3
+end
+
+if argument_format == 3 then
+    error("Wrong usage! Usage is \n\tluajit[32|64] loader.lua library_name global_user_autoload library_user_autoload working_dir filename_base\n\tAlternative usage:\n\tluajit[32|64] loader.lua library_name working_dir filename_base", 1)
 end
 
 -- Define global list of loaded files
@@ -27,10 +35,10 @@ end
 _G.command_line_args = {}
 
 command_line_args.autoload_dir = 		  arg[1]
-command_line_args.global_user_autoload =  arg[2]
-command_line_args.library_user_autoload = arg[3]
-command_line_args.working_dir = 		  arg[4]
-command_line_args.filebase = 			  arg[5]
+command_line_args.global_user_autoload =  argument_format == 1 and arg[2] or "empty"
+command_line_args.library_user_autoload = argument_format == 1 and arg[3] or "empty"
+command_line_args.working_dir = 		  argument_format == 1 and arg[4] or arg[2]
+command_line_args.filebase = 			  argument_format == 1 and arg[5] or arg[3]
 
 -- Autoload the autoload directory content (*.lua)
 load_dir("libraries/" .. command_line_args.autoload_dir)
