ef9a2d651c11e0dfc0b56034a12dc66e0f603d9f
Kai Böse <kai.boese@web.de>
2015-08-05 17:56:48 +0200

    Added polygon function

--- deps/script/bin/libraries/nyufxlua/1_NyuCore.lua
+++ deps/script/bin/libraries/nyufxlua/1_NyuCore.lua
@@ -244,6 +244,27 @@ function shape.heart(size, offset)
 	return shape
 end
 
+function shape.polygon(edges, size)
+	local shape, shape_n = {string.format("m 0 %s l", -size)}, 1
+	local outer_p
+	for i = 1, edges do
+		outer_p = math.rotate({0, -size, 0}, "z", (i / edges)*360)
+		shape_n = shape_n + 1
+		shape[shape_n] = string.format(" %.3f %.3f", outer_p[1], outer_p[2])
+	end
+	shape = table.concat(shape)
+	-- Shift to positive numbers
+	local min_x, min_y = 0, 0
+	shape:gsub("(%-?[%d%.]+)%s+(%-?[%d%.]+)", function(x, y)
+		min_x, min_y = math.min(min_x, x), math.min(min_y, y)
+	end)
+	shape = shape:gsub("(%-?[%d%.]+)%s+(%-?[%d%.]+)", function(x, y)
+		return string.format("%s %s", x-min_x, y-min_y)
+	end)
+	-- Return result
+	return shape
+end
+
 local function shape_glance_or_star(edges, inner_size, outer_size, shape_type)
 	ad = nil
 	if shape_type == "star" then
